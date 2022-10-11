47b3975e8e91f33279082b5f0e8d28326a70a24d
m1shraditya <admtheone196@gmail.com>
2022-10-11 13:21:57 +0530

    moved categories

--- commands/organize.js
+++ commands/organize.js
@@ -1,6 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
+const categories = {
+  media: [".mp4", ".mkv"],
+  archives: [".zip", ".7z", ".rar", ".tar", "gz", ".ar", ".iso", ".xz"],
+  documents: [
+    ".docx",
+    ".doc",
+    ".pdf",
+    ".xlsx",
+    ".xls",
+    ".odt",
+    ".ods",
+    ".odp",
+    ".odg",
+    ".odf",
+    ".txt",
+    ".ps",
+    ".tex",
+  ],
+  app: [".exe", ".dmg", ".pkg", ".deb"],
+};
+
 function organizeFn(srcDir) {
   if (!srcDir) {
     organizeFn(process.cwd());
--- main.js
+++ main.js
@@ -6,27 +6,6 @@ const organizeObj = require('./commands/organize');
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 
-const categories = {
-  media: [".mp4", ".mkv"],
-  archives: [".zip", ".7z", ".rar", ".tar", "gz", ".ar", ".iso", ".xz"],
-  documents: [
-    ".docx",
-    ".doc",
-    ".pdf",
-    ".xlsx",
-    ".xls",
-    ".odt",
-    ".ods",
-    ".odp",
-    ".odg",
-    ".odf",
-    ".txt",
-    ".ps",
-    ".tex",
-  ],
-  app: [".exe", ".dmg", ".pkg", ".deb"],
-};
-
 switch (command) {
   case "tree":
     treeObj.treeKey(inputArr[1], "");
